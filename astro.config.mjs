afe1818d41a6d024e980272b85dcf2fd8e7a48a7
Risav Karna <vasir@fullmingo.>
2024-04-06 00:23:53 +0545

    build(astro): init packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import partytown from "@astrojs/partytown";
+import tailwind from "@astrojs/tailwind";
+import { astroImageTools } from "astro-imagetools";
+import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://anarchitecture.foundation',
+  integrations: [partytown(), tailwind(), astroImageTools, icon(), sitemap(), robotsTxt()]
+});
