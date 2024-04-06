79be4e6bc192947ada47e85c59674dbc8772deba
Risav Karna <vasir@fullmingo.>
2024-04-06 06:59:36 +0545

    build(config): setup more key libs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,11 +8,35 @@ import robotsTxt from "astro-robots-txt";
 import solid from "@astrojs/solid-js";
 import mdx from "@astrojs/mdx";
 import astroI18next from "astro-i18next";
-
 import qwikdev from "@qwikdev/astro";
+import svelte from "@astrojs/svelte";
+import cloudflare from "@astrojs/cloudflare";
+import vercel from "@astrojs/vercel/static";
+import preact from "@astrojs/preact";
+import blog from "astro-blog";
+import tunnel from "astro-tunnel";
+import htmx from 'astro-htmx';
+import matthiesenxyzgists from "@matthiesenxyz/astro-gists";
+import webmanifest from "astro-webmanifest";
+import playformFormat from "@playform/format";
+import playformCompress from "@playform/compress";
+import playformInline from "@playform/inline";
+
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://anarchitecture.foundation',
-  integrations: [partytown(), tailwind(), astroImageTools, icon(), sitemap(), robotsTxt(), mdx(), astroI18next(), solid(), qwikdev()]
+  integrations: [partytown(), tailwind(), astroImageTools, icon(), sitemap(), robotsTxt(), mdx(), astroI18next(), solid(), qwikdev(), svelte(), preact({
+    compat: true
+  }), blog(), tunnel(), matthiesenxyzgists(), htmx(), webmanifest(), playformFormat(), playformCompress(), playformInline(), sentry(), spotlightjs()],
+  output: "static",
+  adapter: vercel({
+    imageService: true,
+    devImageService: 'squoosh',
+    webAnalytics: {
+      enabled: true
+    }
+  })
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,4 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+/// <reference types="../.astro/astro-gists.d.ts" />
+/// <reference types="../.astro/astro-blog.d.ts.d.ts" />
